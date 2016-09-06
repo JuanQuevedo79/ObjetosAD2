e9112dabf8d4e52b56b12c8831415c2bae5f14c1
“Juan” <jmarrugo79@gmail.com>
2016-09-06 10:56:42 -0500

    Inicion del proyecto

---
+++ src/objetosad2/ObjetosAD2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package objetosad2;
+
+/**
+ *
+ * @author jquevedo3
+ */
+public class ObjetosAD2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
