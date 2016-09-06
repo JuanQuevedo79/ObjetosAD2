63bef89573fd070c2253b3e0dbacce6c39e9a635
“Juan” <jmarrugo79@gmail.com>
2016-09-06 11:22:55 -0500

    Interfaz Terminada

--- src/clases/Helper.java
+++ src/clases/Helper.java
@@ -4,19 +4,12 @@
  * and open the template in the editor.
  */
 
-package objetosad2;
+package clases;
 
 /**
  *
  * @author jquevedo3
  */
-public class ObjetosAD2 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Helper {
     
 }
